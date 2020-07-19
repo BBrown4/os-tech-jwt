805d35638afab35562144259926ea4a607994644
Brandon Brown <bmxchamp4@gmail.com>
2020-07-19 16:52:15 -0700

    change api request url to https

--- frontend/src/app/_services/user.service.ts
+++ frontend/src/app/_services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
-const API_URL = 'http://outputstudios.net/api/test/';
+const API_URL = 'https://outputstudios.net/api/test/';
 
 @Injectable({
   providedIn: 'root'
