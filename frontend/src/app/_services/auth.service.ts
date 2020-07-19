7ce61e539f986ab9b72062deb0642648ebcb84a9
Brandon Brown <bmxchamp4@gmail.com>
2020-07-19 16:46:30 -0700

    still trying  to fix stupid mixed content error

--- frontend/src/app/_services/auth.service.ts
+++ frontend/src/app/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 
-const AUTH_API = 'http://outputstudios.net:8080/api/auth/';
+const AUTH_API = 'http://outputstudios.net/api/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
